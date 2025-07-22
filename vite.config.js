7d7086227e415cd1f005ea5db9e71d5152213cae
Isha Sharma <ishasharma.150604@gmail.com>
2025-07-22 10:17:24 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Recipe-Table-React-JS/",
+  base: "/Recipe_Table-in-React/",
   plugins: [react()],
 });
